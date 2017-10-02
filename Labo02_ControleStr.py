68169dfd43462786c01d1d5464d2c61d93c41605
ThomasDepuydt1999 <thomas.depuydt@student.howest.be>
2017-10-02 08:53:18 +0200

    Initial commit

---
+++ Labo02_ControleStr.py
@@ -0,0 +1,9 @@
+#week2 - oef1 - controlestructuren
+#controleer 2 getallen
+getalA = int(input("geef het eerste getal \n"))
+getalB = int(input("geef het tweede getal \n"))
+
+if (getalA == getalB):
+    print("De getallen {0} en  {1} zijn gelijk".format(getalA,getalB))
+else:
+    print("De getallen {0} en  {1} zijn niet gelijk".format(getalA, getalB))
